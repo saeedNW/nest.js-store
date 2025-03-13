f7c50201ce155df0e4f4215d4e4517c83ad81ee8
Saeed Norouzi <saeednorouzi98@gmail.com>
2025-03-14 02:14:18 +0330

    Feat(Main): Register static directory
    Description:
    Register application's static directory.
    
    Logs:
    [Modified][Main] Change Application type.
    [Modified][Main] Register static assets.
    [Modified][Git ignore] Add static directory to ignored directories.

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ import { HttpExceptionFilter } from './common/filters/exception.filter';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create<NestExpressApplication>(AppModule);
+	// Register public folder as static files directory
+	app.useStaticAssets("public");
 	// initialize swagger
 	swaggerConfiguration(app);
 	// initialize custom response interceptor
